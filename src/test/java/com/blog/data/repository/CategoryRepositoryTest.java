6d67c31494a95d6464be4900da70004ba87bdf3e
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-11-11 12:56:15 +0100

    Model

--- src/test/java/com/blog/data/repository/CategoryRepositoryTest.java
+++ src/test/java/com/blog/data/repository/CategoryRepositoryTest.java
@@ -1,7 +1,50 @@
 package com.blog.data.repository;
 
+import com.blog.data.model.Category;
+import com.blog.data.model.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CategoryRepositoryTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    void createCategory(){
+        Category category1 = new Category("Fashion");
+        Category category2 = new Category("Food");
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        assertNotNull(category1);
+        assertNotNull(category2);
+    }
+
+    @Test
+    void updateCategoryById(){
+        Optional<Category> optionalPost = categoryRepository.findById(1L);
+        if(optionalPost.isPresent()){
+            Category category = optionalPost.get();
+            category.setName("Technology");
+            categoryRepository.save(category);
+            assertEquals("Technology", category.getName());
+        }
+    }
+
+    @Test
+    void deleteCategoryById(){
+        Optional<Category> optionalCategory = categoryRepository.findById(2L);
+        if(optionalCategory.isPresent()){
+            Category category = optionalCategory.get();
+            categoryRepository.deleteById(category.getCategoryId());
+            assertEquals(1, categoryRepository.count());
+        }
+    }
 
 }
