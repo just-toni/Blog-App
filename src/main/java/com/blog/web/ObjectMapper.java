a4c35d46cd59d8a38acbe821c6f6317d1680e935
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-11-18 05:19:46 +0100

    Jackson Object Mapper

---
+++ src/main/java/com/blog/web/ObjectMapper.java
@@ -0,0 +1,23 @@
+package com.blog.web;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapper {
+
+    @Bean
+    public ObjectMapper jacksonObjectMapper() {
+
+        return new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                .registerModule(new JavaTimeModule());
+    }
+
+}
